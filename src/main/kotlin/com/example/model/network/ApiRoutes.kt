f26d32b3c7838a077eaf8e22c2d208e8aea7e3a0
rayan_aouf <rayanaouf1512@gmail.com>
2023-11-06 14:45:40 +0100

    alphaMobileAppV0.307

--- src/main/kotlin/com/example/model/network/ApiRoutes.kt
+++ src/main/kotlin/com/example/model/network/ApiRoutes.kt
@@ -2,10 +2,9 @@ package com.example.model.network
 
 
 object ApiRoutes {
-    private const val BASE_URL = "https://biggsamserver.onrender.com"
-    const val getUsers  = "$BASE_URL/getUser"
-    const val signIn    = "$BASE_URL/signIn"
-    const val signUp    = "$BASE_URL/signUp"
-    const val setUp     = "$BASE_URL/setUpAccount"
-    const val getStores = "$BASE_URL/getStores"
+    const val getUsers  = "/getUser"
+    const val signIn    = "/signIn"
+    const val signUp    = "/signUp"
+    const val setUp     = "/setUpAccount"
+    const val getStores = "/getStores"
 }
