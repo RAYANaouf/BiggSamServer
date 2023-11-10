d52c635bbf282eb2241555cf65d10a25e702c9c4
rayan_aouf <rayanaouf1512@gmail.com>
2023-11-10 19:10:25 +0100

    alphaServerV0.311

--- src/main/kotlin/com/example/model/network/ApiRoutes.kt
+++ src/main/kotlin/com/example/model/network/ApiRoutes.kt
@@ -2,9 +2,8 @@ package com.example.model.network
 
 
 object ApiRoutes {
-    const val getUsers  = "/getUser"
-    const val signIn    = "/signIn"
-    const val signUp    = "/signUp"
-    const val setUp     = "/setUpAccount"
+    const val signIn    = "/StoreSignIn"
+    const val signUp    = "/StoreSignUp"
+    const val setUp     = "/StoreSetUpAccount"
     const val getStores = "/getStores"
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -29,33 +29,34 @@ fun Application.configureRouting(database  : Database) {
             call.respondText("hello BiggSam v0.25")
         }
 
-        get("/signIn"){
+        get(ApiRoutes.signIn){
             var email = call.request.headers[ApiHeaders.email]
             var password = call.request.headers[ApiHeaders.password]
 
 
-            var users = database
-                .from(UserEntity)
+            var stores = database
+                .from(AlphaStoreEntity)
                 .select()
                 .where {
-                    (UserEntity.user_email like "$email") and (UserEntity.user_password like "$password")
+                    (AlphaStoreEntity.store_email like "$email") and (AlphaStoreEntity.store_password like "$password")
                 }
 
 
 
-            for (user in users ){
-                if (user.size() > 0){
+            for (store in stores ){
+                if (store.size() > 0){
 
                     call.response.headers.append(ApiHeaders.exception , ApiExceptions.NoException().msg)
 
                     call.respond(
-                        Alpha1User(
-                            user_id = user[UserEntity.user_id]!!,
-                            first_name = user[UserEntity.first_name]!!,
-                            last_name = user[UserEntity.last_name]!!,
-                            phone_number = user[UserEntity.phone_number]!!,
-                            password = user[UserEntity.user_password]!!,
-                            user_email = user[UserEntity.user_email]!!
+                        AlphaStore(
+                            store_id       = store[AlphaStoreEntity.store_id]?.toLong() ?: 0,
+                            store_name     = store[AlphaStoreEntity.store_name]     ?: "",
+                            store_email    = store[AlphaStoreEntity.store_email]    ?: "",
+                            store_photo    = store[AlphaStoreEntity.store_photo]    ?: "",
+                            store_number   = store[AlphaStoreEntity.store_number]   ?: 0 ,
+                            store_wilaya   = store[AlphaStoreEntity.store_wilaya]   ?: "",
+                            store_password = store[AlphaStoreEntity.store_password] ?: ""
                         )
                     )
                 }
@@ -69,18 +70,16 @@ fun Application.configureRouting(database  : Database) {
 
         }
 
-        post("/signUp"){
+        post("/${ApiRoutes.signUp}"){
 
             val signUpRequest = call.receive<SignUpRequest>()
 
-            database.insert(UserEntity){
-                set(it.first_name    , signUpRequest.firstName)
-                set(it.last_name     , signUpRequest.lastName)
-                set(it.phone_number  , signUpRequest.number.toIntOrNull()?: 0)
-                set(it.user_password , signUpRequest.password)
-                set(it.user_email    , signUpRequest.email)
-                set(it.img_uri       , "")
-                set(it.account_type  , "")
+            database.insert(AlphaStoreEntity){
+                set(it.store_name     , signUpRequest.firstName)
+                set(it.store_number   , signUpRequest.number.toIntOrNull()?: 0)
+                set(it.store_password , signUpRequest.password)
+                set(it.store_email    , signUpRequest.email)
+                set(it.store_photo    , "")
             }
 
             var users = database
