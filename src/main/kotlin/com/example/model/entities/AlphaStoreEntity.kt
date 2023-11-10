53b2c358e67283398177858457e2cdd4efe76de2
rayan_aouf <rayanaouf1512@gmail.com>
2023-11-10 18:47:01 +0100

    alphaServerV0.311

--- src/main/kotlin/com/example/model/entities/AlphaStoreEntity.kt
+++ src/main/kotlin/com/example/model/entities/AlphaStoreEntity.kt
@@ -4,7 +4,7 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object AlphaStoreEntity : Table<Nothing>("alphastore") {
+object AlphaStoreEntity : Table<Nothing>("AlphaStore") {
 
     val store_id        = int("store_id").primaryKey()
     val store_name      = varchar("store_name")
