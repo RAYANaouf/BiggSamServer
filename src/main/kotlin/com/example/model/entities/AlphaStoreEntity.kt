95e971ac4dfe3241ed9ea9ca45d3d151ee764ed6
rayan_aouf <rayanaouf1512@gmail.com>
2023-11-10 19:32:31 +0100

    alphaServerV0.313

--- src/main/kotlin/com/example/model/entities/AlphaStoreEntity.kt
+++ src/main/kotlin/com/example/model/entities/AlphaStoreEntity.kt
@@ -10,7 +10,7 @@ object AlphaStoreEntity : Table<Nothing>("alphastore") {
     val store_name      = varchar("store_name")
     val store_photo     = varchar("store_photo")
     val store_email     = varchar("store_email")
-    val store_password  = varchar("password")
+    val store_password  = varchar("store_password")
     val store_number    = int("store_number")
     val store_wilaya    = varchar("store_wilaya")
 
